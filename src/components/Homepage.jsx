421d6f3f8259c630c29dc26928def88d25daebc7
giorgioGTelian <giorgio.programmer@gmail.com>
2024-01-17 15:06:48 +0100

    customize front page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,10 +12,11 @@ function HomePage() {
 
   return (
     <div className="home-page">
-      <JumbotronComponent title="Twitch" onClick={() => navigateToService('/twitch')} />
-      <JumbotronComponent title="Streamyard" onClick={() => navigateToService('/streamyard')} />
-      <JumbotronComponent title="YouTube" onClick={() => navigateToService('/youtube')} />
-      <JumbotronComponent title="Dacast" onClick={() => navigateToService('/dacast')} />
+      <JumbotronComponent title="Twitch" description="twich fornisce solo la piattaforma straming - si appoggia ad altre piattaforme per l'effettiva registrazione - la loro applicazione twich studio è installata localmente nel pc - quindi per creare la registrazione va utilizzato un'altra piattaforma" 
+      imageCard="twitch.png" onClick={() => navigateToService('/twitch')} />
+      <JumbotronComponent title="Streamyard" description="" onClick={() => navigateToService('/streamyard')} />
+      <JumbotronComponent title="YouTube" description="" onClick={() => navigateToService('/youtube')} />
+      <JumbotronComponent title="Dacast" description="" onClick={() => navigateToService('/dacast')} />
     </div>
   );
 }
--- src/components/JumbotronComponent.jsx
+++ src/components/JumbotronComponent.jsx
@@ -1,11 +1,23 @@
 // JumbotronComponent.js
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
 
-function JumbotronComponent({ title, onClick }) {
+function JumbotronComponent({ title, onClick, description, imageCard }) {
     return (
-        <div className="jumbotron" onClick={onClick}>
-        <h1>{title}</h1>
-        </div>
+        <CardGroup>
+            <Card className="d-flex justify-content-center align-items-center" style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={imageCard} style={{ width: '150px', height: '150px', objectFit: 'cover' }}/>
+            <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                {description}
+                </Card.Text>
+                <Button variant="primary">vai a vedere</Button>
+            </Card.Body>
+        </Card>
+    </CardGroup>
     );
 }
 
